fe43af2e2d4e87ca0e6b8f82e684f6545a650dd0
Stephen Allard <stephencallard@gmail.com>
2020-04-23 07:44:24 -0500

    Added an 'insert' method

--- lib/tree.rb
+++ lib/tree.rb
@@ -9,6 +9,7 @@ class Tree
 
   def build_tree(arr, root_node=nil)
     return root_node if arr.empty?
+    arr.uniq! #remove duplicates
     if root_node.nil? 
       root_node = create_node(arr.shift)
     else 
@@ -33,6 +34,32 @@ class Tree
       end
     end
     build_tree(arr, root_node)
+  end
+
+  def insert(value)
+    if root.nil?
+      self.root = create_node(value)
+    else
+      current_node = root
+      new_node = create_node(value)
+      loop do 
+        if new_node < current_node
+          if current_node.left_child.nil?
+            current_node.left_child = new_node
+            break
+          else
+            current_node = current_node.left_child
+          end
+        elsif new_node > current_node
+          if current_node.right_child.nil?
+            current_node.right_child = new_node
+            break
+          else
+            current_node = current_node.right_child
+          end
+        end
+      end
+    end
   end
 
   private
