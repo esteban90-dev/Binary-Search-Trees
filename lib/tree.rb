0227ef014511ece62eacf8f8e3697068ef92acc9
Stephen Allard <stephencallard@gmail.com>
2020-04-24 20:02:26 -0500

    Added a 'parent' method and began working on a 'delete' method

--- lib/tree.rb
+++ lib/tree.rb
@@ -26,6 +26,46 @@ class Tree
     end
   end
 
+  def parent(value)
+    current_node = root
+
+    return nil if current_node.data == value
+
+    loop do
+      return nil if current_node.nil?
+  
+      if current_node.left_child
+        return current_node if current_node.left_child.data == value
+      end
+
+      if current_node.right_child
+        return current_node if current_node.right_child.data == value
+      end
+
+      if value < current_node.data
+        current_node = current_node.left_child
+      elsif value > current_node.data
+        current_node = current_node.right_child
+      end
+    end
+  end
+
+  def delete(value)
+    return nil if root.nil? || find(value).nil?  #return nil if tree is empty or if node doest exist
+    node = find(value)
+
+    #case 1 - leaf node: delete.
+
+    if node.left_child.nil? && node.right_child.nil?
+      puts "leaf node!"
+    end
+
+
+
+    #case 2 - node with 1 subtree: replace current node with next node
+    #case 3 - node with 2 subtrees: replace node with successor node (min val in right subtree)
+  end
+
   def insert(value)
     new_node = create_node(value)
     current_node = root
