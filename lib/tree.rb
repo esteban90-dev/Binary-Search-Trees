9c331ea7e1a28853b662c69d954a2f908fcbcb18
Stephen Allard <stephencallard@gmail.com>
2020-04-27 03:23:37 -0500

    Moved several methods into private

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,27 +2,13 @@ class Tree
   attr_accessor :root
 
   def initialize(input)
-    @root = builder(input)
+    @root = build(input)
   end
 
   public
 
-  def builder(input)
+  def build(input)
     build_tree(input.sort.uniq)
-  end
-
-  def build_tree(arr, root_node=nil)
-    return nil if arr.empty?
-    mid = arr.length / 2
-    left = arr.slice!(0,mid)
-    mid = arr.slice!(0)
-    right = arr
-
-    root_node = create_node(mid)
-    
-    root_node.left_child = build_tree(left, root_node)
-    root_node.right_child = build_tree(right, root_node)
-    return root_node
   end
 
   def insert(value, root_node=root)
@@ -53,7 +39,7 @@ class Tree
   end
 
   def rebalance!(root_node=root)
-    self.root = builder(level_order)
+    self.root = build(level_order)
   end
 
   def find(value, root_node=root)
@@ -79,17 +65,6 @@ class Tree
     else
       return depth_right
     end
-  end
-
-  def get_level(root_node, level, arr=[])
-    return if root_node.nil?
-    if level == 1
-      arr << root_node.data
-    elsif level > 1
-      get_level(root_node.left_child, level - 1, arr)
-      get_level(root_node.right_child, level - 1, arr)
-    end
-    return arr
   end
 
   def level_order
@@ -144,25 +119,6 @@ class Tree
     end
   end
 
-  def parent(value)
-    current_node = root
-    return nil if current_node.data == value
-    loop do
-      return nil if current_node.nil?
-      if current_node.left_child
-        return current_node if current_node.left_child.data == value
-      end
-      if current_node.right_child
-        return current_node if current_node.right_child.data == value
-      end
-      if value < current_node.data
-        current_node = current_node.left_child
-      elsif value > current_node.data
-        current_node = current_node.right_child
-      end
-    end
-  end
-
   def delete(value)
     return nil if root.nil? || find(value).nil?  #return nil if tree is empty or if node doest exist
     node = find(value)
@@ -203,6 +159,41 @@ class Tree
     end
   end
 
+  private
+
+  def build_tree(arr, root_node=nil)
+    return nil if arr.empty?
+    mid = arr.length / 2
+    left = arr.slice!(0,mid)
+    mid = arr.slice!(0)
+    right = arr
+
+    root_node = create_node(mid)
+    
+    root_node.left_child = build_tree(left, root_node)
+    root_node.right_child = build_tree(right, root_node)
+    return root_node
+  end
+
+  def parent(value)
+    current_node = root
+    return nil if current_node.data == value
+    loop do
+      return nil if current_node.nil?
+      if current_node.left_child
+        return current_node if current_node.left_child.data == value
+      end
+      if current_node.right_child
+        return current_node if current_node.right_child.data == value
+      end
+      if value < current_node.data
+        current_node = current_node.left_child
+      elsif value > current_node.data
+        current_node = current_node.right_child
+      end
+    end
+  end
+
   def successor(value)
     return nil if find(value).nil?
     current_node = find(value).right_child
@@ -213,7 +204,16 @@ class Tree
     return current_node
   end
 
-  private
+  def get_level(root_node, level, arr=[])
+    return if root_node.nil?
+    if level == 1
+      arr << root_node.data
+    elsif level > 1
+      get_level(root_node.left_child, level - 1, arr)
+      get_level(root_node.right_child, level - 1, arr)
+    end
+    return arr
+  end
 
   def create_node(value)
     Node.new({
